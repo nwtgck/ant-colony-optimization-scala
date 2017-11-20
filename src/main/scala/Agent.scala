1984086cc9568ef58a813edb6bf525c405df1d8a
Ryo Ota <nwtgck@gmail.com>
2017-11-20 22:05:19 +0900

    [add] Implement Main, Agent

---
+++ src/main/scala/Agent.scala
@@ -0,0 +1,114 @@
+import scala.collection.mutable
+import scala.util.Random
+import scala.util.control.Breaks
+
+class Agent(random: Random, towns: Seq[Int], roads : Map[(Int, Int), Double], start: Int, pheromone: Map[(Int, Int), Double]) {
+  var current: Int    = start
+  var alpha  : Double = 1.0
+  var beta   : Double = 3
+  var Q      : Int    = 100
+
+  val whole    : Seq[Int] = towns
+  var notVisied: Seq[Int] = dropFirstMatch(towns, start)
+  var visited  : Seq[Int] = Seq(towns(start))
+
+  var way      : mutable.Map[(Int, Int), Boolean] = mutable.Map.empty // TODO Not to use mutable
+  for{
+    i <- towns
+    j <- towns
+    if i != j
+  } {
+    way((i, j)) = false
+  }
+
+  def assessment(j: Int): Double = {
+    val numerator  : Double = Math.pow(pheromone(current, j), alpha) * Math.pow(1.0/roads(current, j), beta)
+    val denominator: Double = notVisied.map{l => l
+        Math.pow(pheromone(current, l), alpha) * Math.pow(1.0/roads(current, l), beta)
+    }.sum
+    numerator / denominator
+  }
+
+  def probability: Map[Int, Double] = {
+    val p: mutable.Map[Int, Double] = mutable.Map.empty // TODO Not to use mutable
+    for(m <- notVisied){
+      val mAsses  : Double = assessment(m)
+      val sumAsses: Double = notVisied.map{n => assessment(n)}.sum
+      p(m) = mAsses / sumAsses
+    }
+    // Convert p from mutable to immutable (from: https://stackoverflow.com/a/5050653)
+    Map(p.toSeq: _*)
+  }
+
+  def agentwalk(): Seq[Int] = {
+    for(i <- whole){
+      val prob  : Map[Int, Double] = probability
+      var choice: Double           =  random.nextDouble()
+      var nextT : (Int, Double)    = null // TODO Not to use null
+      val b     : Breaks           = new Breaks
+      b.breakable {
+        for (i <- prob) {
+          choice = choice - i._2
+          if (choice < 0) {
+            nextT = i
+            b.break()
+          }
+        }
+      }
+      way((current, nextT._1)) = true
+      current                  = nextT._1
+      visited   :+= nextT._1
+      notVisied = dropFirstMatch(notVisied, nextT._1)
+
+      if(notVisied.isEmpty){
+        return visited
+      }
+    }
+    visited
+  }
+
+  def getDeltaphero: Map[(Int, Int), Double] = {
+    val sumoflen      : Double = getLength
+    val deltaPheromone: mutable.Map[(Int, Int), Double] = mutable.Map.empty
+    for(i <- pheromone.keys){
+      if(way(i)){
+        deltaPheromone(i) = Q.toDouble / sumoflen
+      } else {
+        deltaPheromone(i) = 0
+      }
+    }
+    Map(deltaPheromone.toSeq: _*)
+  }
+
+  def getPhero(startpheromone: mutable.Map[(Int, Int), Double], pheromones: Seq[Map[(Int, Int), Double]], ro: Double): mutable.Map[(Int, Int), Double] = { // TODO Not to use mutable
+    for(i <- startpheromone.keys){
+      startpheromone(i) = ro * startpheromone(i) + (1.0 - ro) * pheromones.map{k => k(i)}.sum
+    }
+    startpheromone
+  }
+
+  def getLength: Double = {
+    var sumoflen: Double = 0
+    for(i <- 1 until visited.length){
+      sumoflen += roads(visited(i-1), visited(i))
+    }
+    sumoflen
+  }
+
+  def getWay: Seq[Int] = visited
+
+
+  // (from https://alvinalexander.com/scala/how-to-drop-filter-remove-first-matching-element-in-sequence-list)
+  private def dropFirstMatch[A](ls: Seq[A], value: A): Seq[A] = {
+    val index = ls.indexOf(value)  //index is -1 if there is no match
+    if (index < 0) {
+      ls
+    } else if (index == 0) {
+      ls.tail
+    } else {
+      // splitAt keeps the matching element in the second group
+      val (a, b) = ls.splitAt(index)
+      a ++ b.tail
+    }
+  }
+}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,77 @@
+// (from: https://qiita.com/EnsekiTT/items/9b13ceba391221687f42)
+
+import scala.util.Random
+import scala.collection.mutable
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val NumOfTown : Int    = 30
+    val NumOfAgent: Int    = 20
+    val NumOfSolve: Int    = 500
+    val ro        : Double = 0.4
+
+    Random.setSeed(2)
+    val random: Random = new Random()
+
+    val towns     : Seq[Int]        = 0 until NumOfTown
+    val positions : Seq[(Int, Int)] = (0 until NumOfTown).map{i => (random.nextInt(100)+1, random.nextInt(100)+1)}
+
+    val roads    : mutable.Map[(Int, Int), Double] = mutable.Map.empty // TODO Not to use mutable
+    var pheromone: mutable.Map[(Int, Int), Double] = mutable.Map.empty // TODO Not to use mutable
+
+    for{
+      i <- towns
+      j <- towns
+      if i != j
+    } {
+      roads((i, j))     = Math.sqrt( Math.pow(positions(i)._1 - positions(j)._1, 2) + Math.pow(positions(i)._2 - positions(j)._2, 2))
+      pheromone((i, j)) = random.nextDouble()
+    }
+
+    var minLength: Double = Double.MaxValue
+    var bestAgent: Agent  = null // TODO Not to use null
+    var lastPheno: Double = 0.0
+
+    for(i <- 0 until NumOfSolve){
+      var pheros  : Seq[Map[(Int, Int), Double]] = Seq.empty
+      var topAgent: Agent                        = null // TODO Not to use null
+      var k       : Agent                        = null // TODO Not to use null
+      for(m <- 0 until NumOfAgent){
+        k =  new Agent(
+          random,
+          towns=towns,
+          roads=Map(roads.toSeq: _*),
+          start=0,
+          pheromone=Map(pheromone.toSeq: _*)
+        )
+        k.agentwalk()
+        val length = k.getLength
+        pheros :+= k.getDeltaphero
+
+        if(topAgent == null || topAgent.getLength > length){
+          topAgent = k // TODO Deep copy
+        }
+
+        if (minLength == null || minLength > length) {
+          minLength = length
+          bestAgent = k
+        }
+      }
+      println("今" + i + "番目のありんこたちが仕事をしました。")
+      pheromone = k.getPhero(pheromone, pheros, ro)
+
+      if(("%.4f".format(pheromone.values.sum)).toDouble == "%.4f".format(lastPheno).toDouble){
+        pheromone = mutable.Map.empty
+        for{
+          i <- towns
+          j <- towns
+          if i != j
+        } {
+          pheromone((i, j)) = random.nextDouble()
+        }
+      }
+      lastPheno = pheromone.values.sum
+    }
+    println(bestAgent.getWay)
+  }
+}
